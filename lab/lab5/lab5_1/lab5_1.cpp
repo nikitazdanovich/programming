9383d18f60ee78f158b31dd2f027fddb59435e59
nikita_zd <95176240+nikitazdanovich@users.noreply.github.com>
2026-01-14 17:30:44 +0300

    Update lab5_1.cpp

--- lab/lab5/lab5_1/lab5_1.cpp
+++ lab/lab5/lab5_1/lab5_1.cpp
@@ -2,132 +2,131 @@
 
 using namespace std;
 
-enum class weaponType {
+enum class Category {
     ONEHANDED,
     TWOHANDED,
     BOW,
     CROSSBOW,
-    NON
+    NONE
 };
 
-enum class weaponIdc {
-    a,
-    b
+enum class Identifier {
+    A,
+    B
 };
 
-class weapon {
+class Item {
 private:
-    weaponType type;
-    weaponIdc idc;
-    string name;
-    float damage, weight;
+    Category category;
+    Identifier identifier;
+    string title;
+    float power, mass;
     
 public:
-    weapon(string tnam = "NON", float tdam = 0, float twei = 0, 
-           weaponType ttyp = weaponType::NON, weaponIdc tidc = weaponIdc::a) {
-        this->name = tnam;
-        this->damage = tdam;
-        this->weight = twei;
-        this->type = ttyp;
-        this->idc = tidc;
-        cout << "\nWEAPON CREATED;\n";
+    Item(string t = "NONE", float p = 0, float m = 0, 
+         Category c = Category::NONE, Identifier i = Identifier::A) {
+        this->title = t;
+        this->power = p;
+        this->mass = m;
+        this->category = c;
+        this->identifier = i;
+        cout << "\nITEM CREATED;\n";
     }
 
-    ~weapon();
+    ~Item();
     
-    string getName() const;
-    float getWeight() const;
-    virtual float getDamage() const;
+    string getTitle() const;
+    float getMass() const;
+    virtual float getPower() const;
     
-    // 4. Операторы сравнения
-    bool operator>(const weapon& other) const {
-        return this->getDamage() > other.getDamage();
+    bool operator>(const Item& other) const {
+        return this->getPower() > other.getPower();
     }
     
-    bool operator<(const weapon& other) const {
-        return this->getDamage() < other.getDamage();
+    bool operator<(const Item& other) const {
+        return this->getPower() < other.getPower();
     }
     
-    weaponType getType() const;
-    weaponIdc getIdc() const;
-    virtual void attack() = 0;
+    Category getCategory() const;
+    Identifier getIdentifier() const;
+    virtual void use() = 0;
 };
 
-class magicWeapon : public weapon {
+class SpecialItem : public Item {
 private:
-    float addDamage;
+    float extraPower;
     
 public:
-    magicWeapon(string tnam, float tdam, float twei, weaponType ttyp, 
-                weaponIdc tidc, float tadd)
-        : weapon(tnam, tdam, twei, ttyp, tidc), addDamage(tadd) {};
+    SpecialItem(string t, float p, float m, Category c, 
+                Identifier i, float ep)
+        : Item(t, p, m, c, i), extraPower(ep) {};
     
-    magicWeapon() : magicWeapon("EXCALIBUR", 25.7, 237, 
-                                weaponType::BOW, weaponIdc::b, 25) {};
+    SpecialItem() : SpecialItem("Greatbow", 25.7, 237, 
+                                Category::BOW, Identifier::B, 25) {};
     
-    float getAddDamage() const;
-    float getDamage() const override;
-    void attack() override;
+    float getExtraPower() const;
+    float getPower() const override;
+    void use() override;
 };
 
-weapon::~weapon() {
-    cout << "\nWEAPON DESTROYED;\n";
+Item::~Item() {
+    cout << "\nITEM DESTROYED;\n";
 }
 
-class shotWeapon : public weapon {
+class SingleUseItem : public Item {
 private:
-    bool exists;
+    bool used;
     
 public:
-    shotWeapon(string tnam = "NON", float tdam = 0, float twei = 0, 
-               weaponType ttyp = weaponType::NON, weaponIdc tidc = weaponIdc::a, 
-               bool texi = 0) 
-        : weapon(tnam, tdam, twei, ttyp, tidc), exists(texi) {};
+    SingleUseItem(string t = "NONE", float p = 0, float m = 0, 
+               Category c = Category::NONE, Identifier i = Identifier::A, 
+               bool u = 0) 
+        : Item(t, p, m, c, i), used(u) {};
     
-    bool getExists() const;
-    void attack() override;
+    bool isUsed() const;
+    void use() override;
 };
 
-string weapon::getName() const {
-    return name;
+string Item::getTitle() const {
+    return title;
 }
 
-float weapon::getDamage() const {
-    return damage;
+float Item::getPower() const {
+    return power;
 }
 
-float weapon::getWeight() const {
-    return weight;
+float Item::getMass() const {
+    return mass;
 }
 
-weaponType weapon::getType() const {
-    return type;
+Category Item::getCategory() const {
+    return category;
 }
 
-weaponIdc weapon::getIdc() const {
-    return idc;
+Identifier Item::getIdentifier() const {
+    return identifier;
 }
 
-float magicWeapon::getAddDamage() const {
-    return addDamage;
+float SpecialItem::getExtraPower() const {
+    return extraPower;
 }
 
-float magicWeapon::getDamage() const {
-    return weapon::getDamage() + addDamage;
+float SpecialItem::getPower() const {
+    return Item::getPower() + extraPower;
 }
 
-bool shotWeapon::getExists() const {
-    return exists;
+bool SingleUseItem::isUsed() const {
+    return used;
 }
 
-void magicWeapon::attack() {
-    cout << "\nMagic attack\n";
+void SpecialItem::use() {
+    cout << "\nSpecial attack\n";
 }
 
-void shotWeapon::attack() {
-    if (getExists() == false) {
+void SingleUseItem::use() {
+    if (isUsed() == false) {
         cout << "Single use attack" << endl;
-        this->exists = true;
+        this->used = true;
     }
     else {
         cout << "Already used" << endl;
@@ -137,52 +136,51 @@ void shotWeapon::attack() {
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    magicWeapon mw("Magic Sword", 30, 5, weaponType::ONEHANDED, 
-                   weaponIdc::a, 15);
+    SpecialItem item1("Special Sword", 30, 5, Category::ONEHANDED, 
+                   Identifier::A, 15);
     
-    cout << "Magic weapon: " << mw.getName() << endl;
-    cout << "Base damage: " << mw.weapon::getDamage() << endl;
-    cout << "Extra damage: " << mw.getAddDamage() << endl;
-    cout << "Total damage: " << mw.getDamage() << endl;
+    cout << "Item: " << item1.getTitle() << endl;
+    cout << "Base power: " << item1.Item::getPower() << endl;
+    cout << "Extra power: " << item1.getExtraPower() << endl;
+    cout << "Total power: " << item1.getPower() << endl;
     
-    class TestWeapon : public weapon {
+    class TestItem : public Item {
     public:
-        TestWeapon(string n, float d) : weapon(n, d, 1, weaponType::ONEHANDED) {}
-        void attack() override {}
+        TestItem(string n, float p) : Item(n, p, 1, Category::ONEHANDED) {}
+        void use() override {}
     };
     
-    TestWeapon sword("Sword", 35);
-    magicWeapon magicAxe("Magic Axe", 25, 6, weaponType::TWOHANDED, 
-                         weaponIdc::b, 20);  // Итого 45 урона
+    TestItem item2("Sword", 35);
+    SpecialItem item3("Special Axe", 25, 6, Category::TWOHANDED, 
+                         Identifier::B, 20);
     
-    cout << "Sword damage: " << sword.getDamage() << endl;
-    cout << "Magic Axe total damage: " << magicAxe.getDamage() << endl;
+    cout << "Sword power: " << item2.getPower() << endl;
+    cout << "Special Axe total power: " << item3.getPower() << endl;
     
-    if (magicAxe > sword) {
-        cout << magicAxe.getName() << " > " << sword.getName() 
-             << " (" << magicAxe.getDamage() << " > " << sword.getDamage() << ")" << endl;
+    if (item3 > item2) {
+        cout << item3.getTitle() << " > " << item2.getTitle() 
+             << " (" << item3.getPower() << " > " << item2.getPower() << ")" << endl;
     } else {
-        cout << magicAxe.getName() << " <= " << sword.getName() 
-             << " (" << magicAxe.getDamage() << " <= " << sword.getDamage() << ")" << endl;
+        cout << item3.getTitle() << " <= " << item2.getTitle() 
+             << " (" << item3.getPower() << " <= " << item2.getPower() << ")" << endl;
     }
     
-    if (sword < magicAxe) {
-        cout << sword.getName() << " < " << magicAxe.getName() 
-             << " (" << sword.getDamage() << " < " << magicAxe.getDamage() << ")" << endl;
+    if (item2 < item3) {
+        cout << item2.getTitle() << " < " << item3.getTitle() 
+             << " (" << item2.getPower() << " < " << item3.getPower() << ")" << endl;
     }
     
-    // Ещё тесты
-    magicWeapon staff("Staff", 20, 3, weaponType::TWOHANDED, weaponIdc::a, 10);
-    TestWeapon bow("Bow", 28);
+    SpecialItem item4("Staff", 20, 3, Category::TWOHANDED, Identifier::A, 10);
+    TestItem item5("Bow", 28);
     
-    cout << "\nStaff total damage: " << staff.getDamage() << endl;
-    cout << "Bow damage: " << bow.getDamage() << endl;
+    cout << "\nStaff total power: " << item4.getPower() << endl;
+    cout << "Bow power: " << item5.getPower() << endl;
     
-    if (staff > bow) {
+    if (item4 > item5) {
         cout << "Staff stronger than Bow" << endl;
     }
     
-    if (bow < staff) {
+    if (item5 < item4) {
         cout << "Bow weaker than Staff" << endl;
     }
     
