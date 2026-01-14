f295d956582e7e8c975a790b792b32e5460f72e6
nikita_zd <95176240+nikitazdanovich@users.noreply.github.com>
2026-01-14 11:12:04 +0300

    Update lab4_1.cpp

--- lab/lab4/lab4_1/lab4_1.cpp
+++ lab/lab4/lab4_1/lab4_1.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Weapon {
+private:
+    string name;
+    float damage;
+    float weight;
+
+public:
+    Weapon(string n, float d, float w) {
+        name = n;
+        damage = d;
+        weight = w;
+    }
+
+    // Конструктор без параметров
+    Weapon() : Weapon("Меч", 7.5f, 3.5f) {}
+
+    string getName() { return name; }
+    float getDamage() { return damage; }
+    float getWeight() { return weight; }
+};
+
+int main() {
+    // Создаём 2 объекта
+    Weapon weapon1("Нож", 5.0f, 0.5f);
+    Weapon weapon2;
+    
+    // Выводим значения
+    cout << "Оружие 1:" << endl;
+    cout << "Название: " << weapon1.getName() << endl;
+    cout << "Урон: " << weapon1.getDamage() << endl;
+    cout << "Вес: " << weapon1.getWeight() << endl;
+    
+    cout << "\nОружие 2:" << endl;
+    cout << "Название: " << weapon2.getName() << endl;
+    cout << "Урон: " << weapon2.getDamage() << endl;
+    cout << "Вес: " << weapon2.getWeight() << endl;
+    
+    return 0;
+}
