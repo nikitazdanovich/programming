21897dc8b518bd3b7351e48a216b9e32c2ab8f04
nikita_zd <95176240+nikitazdanovich@users.noreply.github.com>
2026-01-14 11:36:56 +0300

    Create MyMath.h

---
+++ lab/lab4/lab4_4/MyMath.h
@@ -0,0 +1,12 @@
+#pragma once
+class MyMath
+{
+public:
+
+	static int counter;
+
+	static float Add(float a, float b);
+	static float Sub(float a, float b);
+	static float Mult(float a, float b);
+	static float Div(float a, float b);
+};
